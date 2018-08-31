f08c97b787d60f496d9b254c4919356e07f71b33
Ranyelson Dantas <nielsonnp@gmail.com>
2018-08-31 11:52:31 -0300

    Exercicios_05

--- 005_AntcessorSucessor.py
+++ 005_AntcessorSucessor.py
@@ -3,5 +3,5 @@ s = num_1 + 1
 a = num_1 - 1
 print('Analisando o número {}, seu antecessor é {} e o seu sucessor é {}'.format(num_1,a,s))
 
-'''Ou desse jeito '''
-'''print('O número digitado foi', num_1,'seu antecessor é', a, 'e o seu sucessor é', s)'''
+''' Ou desse jeito '''
+''' print('O número digitado foi', num_1,'seu antecessor é', a, 'e o seu sucessor é', s) '''
