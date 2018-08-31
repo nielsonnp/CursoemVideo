dcd9a0531c83d1d93e8cb0468b0987fdfec2856b
Ranyelson Dantas <nielsonnp@gmail.com>
2018-08-31 11:52:49 -0300

    Exercicios_06

--- 006_Dobro_Triplo_RaizQ.py
+++ 006_Dobro_Triplo_RaizQ.py
@@ -5,6 +5,6 @@ r = n1 ** (1/2)
 
 print('O dobro de {} é {}, \no triplo é {} \ne a raiz quadrada é {:.2f}'.format(n1, d, t, r))
 
-''' \n quebra de linha '''
+'''\n quebra de linha'''
 ''' {:.2f} é para diminuir as casas decimais '''
 ''' {:=^20} pra deixar o sinal de igual e o numero dentro de 20 caracteres '''
