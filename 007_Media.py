fc4bb438ef7623c6d51ee74ec994b7910b6f5c44
Ranyelson Dantas <nielsonnp@gmail.com>
2018-08-31 11:45:47 -0300

    Exercicios_07

--- 007_Media.py
+++ 007_Media.py
@@ -3,7 +3,7 @@ n2 = float(input('Digite a nota 2: '))
 
 print('A média entre {} e {} = {}'.format(n1, n2, (n1+n2)/2))
 
-''' Ou declarando a variavel m'''
+''' Ou declarando a variavel m '''
 
 ''' m = (n1+n2)/2 '''
 ''' print('A média entre {} e {} = {}'.format(n1, n2, m)) '''
