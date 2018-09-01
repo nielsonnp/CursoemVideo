aea019170b459d01b1409beb25b8efd6677111fd
Ranyelson Dantas <nielsonnp@gmail.com>
2018-09-01 11:15:04 -0300

    Exercicios_08

--- 008_ConversorMedida.py
+++ 008_ConversorMedida.py
@@ -3,4 +3,4 @@ v_metro = float(input('Digite um valor em metros: '))
 v_cm = v_metro*100
 v_mm = v_metro*1000
 
-print('A medida de {}m correspone a {:.0f}cm e {:.0f}mm'.format(v_metro, v_cm, v_mm))
+print('A medida de {}m corresponde a {:.0f}cm e {:.0f}mm'.format(v_metro, v_cm, v_mm))
